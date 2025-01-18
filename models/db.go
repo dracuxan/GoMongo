68bc0ed2ef4ece3e6fc1ead7cdbc7ef234af1388
Nisarg <khodkenisarg@gmail.com>
2025-01-18 10:02:26 +0530

    add

--- models/db.go
+++ models/db.go
@@ -1,1 +1,16 @@
 package models
+
+import "gopkg.in/mgo.v2/bson"
+
+type User struct {
+	Id            bson.ObjectId  `json:"id"            bson:"_id"`
+	Name          string         `json:"name"          bson:"name"`
+	Email         string         `json:"email"         bson:"email"`
+	Role          string         `json:"role"          bson:"role"`
+	Subscriptions []Subscription `json:"subscriptions" bson:"subscriptions"`
+}
+
+type Subscription struct {
+	Name  string  `json:"name"  bson:"name"`
+	Price float64 `json:"price" bson:"price"`
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,27 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gopkg.in/mgo.v2"
 
 	"github.com/dracuxan/GoMongo/controllers"
 )
 
+var url string = "mongodb://localhost:27107"
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
 func Routes(app *fiber.App) {
-	app.Get("/", controllers.Home)
+	uc := controllers.NewUserController(getSession())
+
+	app.Get("/users", uc.GetUsers)
+	app.Get("/user/:id", uc.GetUser)
+	app.Post("/user", uc.CreateUser)
+	app.Delete("/user/:id", uc.DeleteUser)
 }
