8a3ade1c27f589e5308fa4a039d6cd36ae248da8
Nisarg <khodkenisarg@gmail.com>
2025-01-30 21:33:52 +0530

    upd + fix

--- controllers/handlers.go
+++ controllers/handlers.go
@@ -22,15 +22,20 @@ func NewUserController(db *mongo.Database) *UserController {
 
 func (uc *UserController) GetUser(c *fiber.Ctx) error {
 	id := c.Params("id")
+
 	objeID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid user ID"})
 	}
+
 	u := models.User{}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	err = uc.Database.Collection("users").FindOne(ctx, bson.M{"_id": objeID}).Decode(&u)
+	if err != nil {
+		return c.SendStatus(fiber.StatusNotFound)
+	}
 
 	return c.Status(fiber.StatusOK).JSON(u)
 }
@@ -38,16 +43,23 @@ func (uc *UserController) GetUser(c *fiber.Ctx) error {
 func (uc *UserController) GetUsers(c *fiber.Ctx) error {
 	var users []models.User
 	collection := uc.Database.Collection("users") // Ensure collection name matches
-	filter := bson.D{}
+	filter := bson.M{}                            // Empty filter to fetch all documents
 
-	curr, err := collection.Find(context.Background(), filter)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Ensure you're properly calling Find with the correct context
+	curr, err := collection.Find(ctx, filter)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).
-			JSON(fiber.Map{"error": "failed to fetch users"})
+			JSON(fiber.Map{"error": "failed to fetch users", "details": err.Error()})
 	}
-	defer curr.Close(context.Background())
 
-	for curr.Next(context.Background()) {
+	// Defer closing the cursor using the same context instead of background
+	defer curr.Close(ctx) // Changed from context.Background() to ctx
+
+	// Iterate over the cursor using the same context
+	for curr.Next(ctx) { // Changed to use the correct ctx here
 		var result models.User
 		if err := curr.Decode(&result); err != nil {
 			return c.Status(fiber.StatusInternalServerError).
@@ -56,16 +68,38 @@ func (uc *UserController) GetUsers(c *fiber.Ctx) error {
 		users = append(users, result)
 	}
 
+	// Check for any errors after iterating over the cursor
 	if err := curr.Err(); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "cursor error"})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{"error": "cursor error"})
 	}
 
+	// Return the list of users as a JSON response
 	return c.Status(fiber.StatusOK).JSON(users)
 }
 
 func (uc *UserController) CreateUser(c *fiber.Ctx) error {
-	// u := models.User{}
-	return nil
+	user := models.User{}
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).
+			JSON(fiber.Map{"error": "unable to parse json body"})
+	}
+
+	if user.Id.IsZero() {
+		user.Id = primitive.NewObjectID()
+	}
+
+	coll := uc.Database.Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := coll.InsertOne(ctx, &user)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{"error": "failed to insert user", "details": err.Error()})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"id": res.InsertedID})
 }
 
 func (uc *UserController) UpdateUser(c *fiber.Ctx) error {
--- models/db.go
+++ models/db.go
@@ -1,13 +1,15 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	Id            bson.ObjectId  `json:"id"            bson:"_id"`
-	Name          string         `json:"name"          bson:"name"`
-	Email         string         `json:"email"         bson:"email"`
-	Role          string         `json:"role"          bson:"role"`
-	Subscriptions []Subscription `json:"subscriptions" bson:"subscriptions"`
+	Id            primitive.ObjectID `json:"id"            bson:"_id"`
+	Name          string             `json:"name"          bson:"name"`
+	Email         string             `json:"email"         bson:"email"`
+	Role          string             `json:"role"          bson:"role"`
+	Subscriptions []Subscription     `json:"subscriptions" bson:"subscriptions"`
 }
 
 type Subscription struct {
